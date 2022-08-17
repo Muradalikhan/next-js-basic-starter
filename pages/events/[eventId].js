8be9039cf8400bfb29b31cdce25e06e1b3cf8a00
Murad khan <murad.jaddoon@gmail.com>
2022-08-17 20:03:13 +0500

    event card created

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../component/event-detail/event-summary";
+import EventLogistics from "../../component/event-detail/event-logistics";
+import EventContent from "../../component/event-detail/event-content";
 
 const EventDetails = () => {
-  return <div>EventDetails</div>;
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>Event not found</p>;
+  }
+  return (
+    <Fragment>
+      <EventSummary title={event.title}/>
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
 };
 
 export default EventDetails;
