2db3cd2ea75b6b03a3b2aea2d2b7e98f3f1722c9
Murad khan <murad.jaddoon@gmail.com>
2022-08-19 20:23:32 +0500

    fetch data from dummy json in getStaticProps function

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,24 @@
-import Head from "next/head";
-import Navbar from "../component/Navbar";
+import path from "path";
+import fs from "fs/promises";
 
-export default function Home() {
+export default function Home(props) {
+  const { products } = props;
   return (
-    <>
-      <Head>
-        <title>next js</title>
-      </Head>
-      <h1>Welcome to Next js Home!</h1>
-    </>
+    <ul>
+      {products.map((item) => (
+        <li key={item.id}>{item.title}</li>
+      ))}
+    </ul>
   );
+}
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-data.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+  return {
+    props: {
+      products: data.products,
+    },
+  };
 }
