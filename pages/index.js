e2cd3b0ef9a013c911559174dd86c4e852f6604a
Murad khan <murad.jaddoon@gmail.com>
2022-08-20 23:50:29 +0500

    link is imported

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,42 @@
 import path from "path";
 import fs from "fs/promises";
+import Link from "next/link";
 
 export default function Home(props) {
   const { products } = props;
   return (
     <ul>
       {products.map((item) => (
-        <li key={item.id}>{item.title}</li>
+        <li key={item.id}>
+          <Link href={item.id}>{item.title}</Link>
+        </li>
       ))}
     </ul>
   );
 }
 
 export async function getStaticProps() {
+  console.log("Regenrating ...")
   const filePath = path.join(process.cwd(), "data", "dummy-data.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+
+  if(!data){
+    return{
+      redirect:{
+        destination:"no-data"
+      }
+    }
+  }
+  if(data.products.length===0){
+    return{
+      noFound:true
+    }
+  }
   return {
     props: {
       products: data.products,
     },
+    revalidate:5
   };
 }
